3ce8b1341be8d731c3f9767a392fd2ae0f1d0617
patrick brisbin <pbrisbin@gmail.com>
2013-11-02 17:09:28 -0400

    Basic if statement parsing

--- Text/Liquid/Parse.hs
+++ Text/Liquid/Parse.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE FlexibleContexts  #-}
-module Text.Liquid.Parse
-    ( Template
-    , TPart(..)
-    , parseTemplate
-    ) where
+module Text.Liquid.Parse where
+    -- ( Template
+    -- , TPart(..)
+    -- , TPredicate(..)
+    -- , parseTemplate
+    -- ) where
 
 import Control.Arrow
 import Data.Text (Text)
@@ -17,80 +18,124 @@ type Template = [TPart]
 
 data TPart = TString Text
            | TFor Text Text Template
+           | TIf TPredicate Template (Maybe Template)
            | TVar Text deriving (Show, Eq)
+
+data TPredicate = TTruthy Text
+                | TEquals Text Text
+                | TNotEquals Text Text
+                | TGreaterEquals Text Text
+                | TLessEquals Text Text
+                | TGreater Text Text
+                | TLess Text Text deriving (Show, Eq)
 
 parseTemplate :: Text -> Either String Template
 parseTemplate = left show . parse template ""
 
 template :: Parser Template
-template = manyTill tpart eof'
+template = manyTill tpart eof
 
 tpart :: Parser TPart
-tpart = try special <|> text
+tpart = special <|> text
 
 special :: Parser TPart
-special = try interpolation <|> forLoop
+special =   try interpolation
+        <|> try forLoop
+        <|> ifStatement
 
 interpolation :: Parser TPart
-interpolation = do
-    str <- between (string "{{")
-                   (string "}}")
-                   (stripped variable)
-
-    return $ TVar (T.pack str)
-
-    where
+interpolation = fmap (TVar . T.pack)
+              $ between (string "{{") (string "}}")
+                        (stripped variable)
 
 forLoop :: Parser TPart
 forLoop = do
-    openFor
-    many space
+    openTag "for"
     elem <- variable
     stripped $ string "in"
     list <- variable
     many space
     string "%}"
 
-    inner <- manyTill tpart (try endFor)
+    inner <- manyTill tpart (try $ endTag "for")
 
     return $ TFor (T.pack elem) (T.pack list) inner
 
+ifStatement :: Parser TPart
+ifStatement = do
+    openTag "if"
+    pred <- predicate
+    many space
+    string "%}"
+
+    cons <- manyTill tpart (try $ endTag "if")
+
+    -- TODO: Alternate
+    return $ TIf pred cons Nothing
+
+predicate :: Parser TPredicate
+predicate =  try binaryPredicate
+         <|> truthyPredicate
+
+binaryPredicate :: Parser TPredicate
+binaryPredicate = do
+    lhs <- try quotedString <|> variable
+    op  <- operator
+    rhs <- try quotedString <|> variable
+
+    return $
+        case op of
+            "==" -> TEquals (T.pack lhs) (T.pack rhs)
+            "!=" -> TNotEquals (T.pack lhs) (T.pack rhs)
+            ">=" -> TGreaterEquals (T.pack lhs) (T.pack rhs)
+            "<=" -> TLessEquals (T.pack lhs) (T.pack rhs)
+            ">"  -> TGreater (T.pack lhs) (T.pack rhs)
+            "<"  -> TLess (T.pack lhs) (T.pack rhs)
+
     where
-        openFor = do
-            string "{%"
-            many space
-            string "for"
+        quotedString = do
+            c   <- oneOf "\'\""
+            str <- manyTill anyChar $ char c
+
+            return str
+
+        operator =   stripped
+                 $   try (string "==")
+                 <|> try (string "!=")
+                 <|> try (string ">=")
+                 <|> try (string "<=")
+                 <|> try (string ">")
+                 <|> string "<"
+
+truthyPredicate :: Parser TPredicate
+truthyPredicate = fmap (TTruthy . T.pack) variable
 
 text :: Parser TPart
-text = do
-    value <- manyTill anyToken $ ended
-
-    return $ TString (T.pack value)
+text = fmap (TString . T.pack) $ manyTill anyToken eot
 
     where
-        ended = lookAhead $ try special <|> try ender <|> eof'
+        eot = lookAhead $   try (string "{{" >> return ())
+                        <|> try (string "{%" >> return ())
+                        <|> eof
 
 variable :: Parser String
-variable = many $ letter <|> char '_' <|> char '.'
+variable = many $   letter
+                <|> char '_'
+                <|> char '.'
 
-endFor :: Parser ()
-endFor = do
-    between (string "{%")
-            (string "%}")
-            (stripped $ string "endfor")
+openTag :: String -> Parser ()
+openTag tg = do
+    string "{%"
+    stripped (string tg)
 
     return ()
 
--- | Result of this parser should always be discarded. It returns an
---   empty TPart so it can be used as a section delimiter in text.
-ender :: Parser TPart
-ender = do
-    endFor -- <|> endWhatever
+endTag :: String -> Parser ()
+endTag tg = do
+    between (string "{%") (string "%}")
+            (stripped $ string $ "end" ++ tg)
 
-    return $ TString ""
+    return ()
 
 stripped :: Stream s m Char => ParsecT s u m b -> ParsecT s u m b
 stripped = between (many space) (many space)
-
-eof' :: Parser TPart
-eof' = eof >> return (TString "")
--- spec/Parse.hs
+++ spec/Parse.hs
@@ -15,12 +15,32 @@ main = hspec $ do
 
         it "parses a for loop" $ do
             "{% for post in posts %}\nTitle: {{post.title}}\n{% endfor %}"
-                    `shouldParseTo` [ TFor "post" "posts"
-                                        [ TString "\nTitle: "
-                                        , TVar "post.title"
-                                        , TString "\n"
-                                        ]
+                `shouldParseTo` [ TFor "post" "posts"
+                                    [ TString "\nTitle: "
+                                    , TVar "post.title"
+                                    , TString "\n"
                                     ]
+                                ]
+
+        it "parses if statements with equality" $ do
+            "oh {% if user.name == 'elvis presley' %}\nhey {{king}}.\n{% endif %}"
+                `shouldParseTo` [ TString "oh "
+                                , TIf (TEquals "user.name" "elvis presley")
+                                    [ TString "\nhey "
+                                    , TVar "king"
+                                    , TString ".\n"
+                                    ] Nothing
+                                ]
+
+        it "parses if statements with truthy tests" $ do
+            "oh {% if user.name %}\nhey {{king}}.\n{% endif %}"
+                `shouldParseTo` [ TString "oh "
+                                , TIf (TTruthy "user.name")
+                                    [ TString "\nhey "
+                                    , TVar "king"
+                                    , TString ".\n"
+                                    ] Nothing
+                                ]
 
     where
         str `shouldParseTo` ast = parseTemplate str `shouldBe` Right ast
